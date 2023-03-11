dff65d20df3b7d4bc8cb72264fd9633aed92d49e
quive <unjusodal@gmail.com>
2023-03-11 11:49:37 +0400

    getting data from the backend now + loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import React from 'react'
+
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
-import Pizza from './components/Pizza'
-
-import pizzas from './assets/pizzas.json'
+import Pizza from './components/Pizza/Pizza'
+import PizzaSkeleton from './components/Pizza/PizzaSkeleton'
 
 import './scss/app.scss'
 
 function App() {
+	const [pizzas, setPizzas] = React.useState([])
+	const [isLoading, setIsLoading] = React.useState(true)
+
+	React.useEffect(() => {
+		fetch('https://640b7d8b65d3a01f981c3e71.mockapi.io/items')
+			.then(res => res.json())
+			.then(json => {
+				setPizzas(json)
+				setIsLoading(false)
+			})
+	}, [])
+
 	return (
 		<div className='wrapper'>
 			<Header />
@@ -19,9 +32,9 @@ function App() {
 					</div>
 					<h2 className='content__title'>Все пиццы</h2>
 					<div className='content__items'>
-						{pizzas.map(pizza => (
-							<Pizza key={pizza.id} {...pizza} />
-						))}
+						{isLoading
+							? [...new Array(8)].map((_, index) => <PizzaSkeleton key={index}/>)
+							: pizzas.map(pizza => <Pizza key={pizza.id} {...pizza} />)}
 					</div>
 				</div>
 			</div>
